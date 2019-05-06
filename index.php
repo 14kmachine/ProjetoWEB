2abae4533f51e70c23d58f65d612d12c8fd17efe
gabriel.vteodoro <gabriel.vteodoro@BOT0626768W10-1>
2019-05-06 14:51:17 -0300

    Criação da pagina Quem Somos

--- index.php
+++ index.php
@@ -43,8 +43,8 @@
             <div class="navbar navbar-default">
                 <div class="container-fluid">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Inicio</a></li>
-                        <li><a href="#">Quem Somos</a></li>
+                        <li class="active"><a href="index.php">Inicio</a></li>
+                        <li><a href="QuemSomos.php">Quem Somos</a></li>
                         <li><a href="#">Historia</a></li>
                         <li><a href="#">Noticias</a></li>
                         <li><a href="#">Contato</a></li>
